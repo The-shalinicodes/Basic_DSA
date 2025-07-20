8fad882b3115d4dcd12bc061300fa628dd67b99b
Shalu <shlokashalini@gmail.com>
2025-07-20 22:23:21 +0530

    Added Reverse_NUM

---
+++ reverseNumber.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int reverseNumber(int n) {
+    int revNum = 0;
+    while (n > 0) {
+      int lastDigit = n % 10;
+      revNum = (revNum * 10) + lastDigit;
+      n = n / 10;
+    }
+    return revNum;
+  }
+}
