6ad2158a638c5315d9ceb4b46b26e20b8a13dd33
Shalu <shlokashalini@gmail.com>
2025-08-09 20:42:50 +0530

    Added selectionSort Solution

---
+++ selectionSort.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] selectionSort(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            int minIndex=i;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]<nums[minIndex]){
+                minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                int temp=nums[i];
+                nums[i]=nums[minIndex];
+                nums[minIndex]=temp;
+            }
+        }
+        return nums;
+    }
+}
