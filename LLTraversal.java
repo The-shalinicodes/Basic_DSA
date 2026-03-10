465662f010ad8fdc75c5b0bfe5f83f9ddb0e2acf
Shalu <shlokashalini@gmail.com>
2026-03-10 13:17:09 +0530

    Added LLTraversal Solution

---
+++ LLTraversal.java
@@ -0,0 +1,12 @@
+lass Solution {
+  public List<Integer> LLTraversal(ListNode head) {
+    // your code goes here
+    ListNode temp = head;
+    List<Integer> ans = new ArrayList<>();
+    while (temp != null) {
+      ans.add(temp.data);
+      temp = temp.next;
+    }
+    return ans;
+  }
+}
